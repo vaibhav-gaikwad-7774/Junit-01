c3c9f50b026fc619adb37edff3313df7e29c180f
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2024-12-29 21:22:14 +0530

    java Telusko Junit files on 29 dec

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		System.out.println("First Junit Test Case Passed");
+	}
+
+}
