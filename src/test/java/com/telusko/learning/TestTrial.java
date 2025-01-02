714c23ffedd979d2a8dcf444c97af93f2123b92b
vaibhav-gaikwad-7774 <vaibhavgaikwad7774@gmail.com>
2025-01-02 21:16:17 +0530

    2nd Dec 2025 night

---
+++ src/test/java/com/telusko/learning/TestTrial.java
@@ -0,0 +1,14 @@
+package com.telusko.learning;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTrial {
+
+	@Test
+	void test() {
+		assertEquals(6, 3+3);
+	}
+
+}
